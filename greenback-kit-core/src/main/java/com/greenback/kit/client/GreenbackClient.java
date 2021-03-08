897060caea236f87606721431d190146e11991dc
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-03-08 12:22:44 +0000

    update'

--- greenback-kit-core/src/main/java/com/greenback/kit/client/GreenbackClient.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/client/GreenbackClient.java
@@ -1,1 +1,8 @@
-package com.green
+package com.greenback.kit.client;
+
+import com.greenback.kit.model.AutoExportQuery;
+import com.greenback.kit.model.*;
+
+import java.io.IOException;
+
+public interface 
