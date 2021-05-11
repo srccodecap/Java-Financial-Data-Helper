1cd4bd66b4b6c7e19300d7239eb3941920f9f237
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-05-11 17:30:07 +0000

    v5.0.5

--- greenback-kit-core/src/main/java/com/greenback/kit/client/GreenbackCodec.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/client/GreenbackCodec.java
@@ -2,4 +2,12 @@ package com.greenback.kit.client;
 
 import com.greenback.kit.model.*;
 
-import java.io.IOException
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public interface GreenbackCodec {
+
+    String prettyPrint(Object value) throws IOException;
+    
+   
