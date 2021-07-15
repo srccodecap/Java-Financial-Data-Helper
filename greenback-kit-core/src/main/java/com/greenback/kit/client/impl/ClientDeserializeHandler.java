10f2412a45601e2262168106f3b4f80479ade9b9
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-07-15 21:02:38 +0000

    test?

--- greenback-kit-core/src/main/java/com/greenback/kit/client/impl/ClientDeserializeHandler.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/client/impl/ClientDeserializeHandler.java
@@ -3,3 +3,9 @@ package com.greenback.kit.client.impl;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+public interface ClientDeserializeHandler<T> {
+
+    public T apply(InputStream input) throws IOException;
+    
+}
---
+++ greenback-kit-core/src/main/java/com/greenback/kit/client/impl/ClientHelper.java
@@ -0,0 +1,5 @@
+package com.greenback.kit.client.impl;
+
+import com.fizzed.crux.uri.MutableUri;
+import com.greenback.kit.client.GreenbackCodec;
+import com.greenback.kit.model.
