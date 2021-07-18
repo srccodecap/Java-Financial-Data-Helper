d10c7d25592115092eca6cbc51ee31286786a250
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-07-18 16:15:35 +0000

    Error handling improvements

--- greenback-kit-core/src/main/java/com/greenback/kit/client/impl/ClientHelper.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/client/impl/ClientHelper.java
@@ -2,4 +2,8 @@ package com.greenback.kit.client.impl;
 
 import com.fizzed.crux.uri.MutableUri;
 import com.greenback.kit.client.GreenbackCodec;
-import com.greenback.kit.model.
+import com.greenback.kit.model.GreenbackException;
+import com.greenback.kit.model.Paginated;
+import com.greenback.kit.model.Query;
+import com.greenback.kit.util.IoFunction;
+imp
