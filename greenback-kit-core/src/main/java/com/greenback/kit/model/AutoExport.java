63f8418e6575ba4b64afc61322b34f1b131b0f4d
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-11-09 05:24:54 +0000

    Promote RDO repository to latest consistent URL

--- greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExport.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExport.java
@@ -3,4 +3,8 @@ package com.greenback.kit.model;
 import java.util.List;
 import java.time.Instant;
 
-public class AutoExport extends GreenbackObje
+public class AutoExport extends GreenbackObject {
+    private String userId;
+    private String accountingAccountId;
+    private AutoExportParameters parameters;
+    private AutoEx
