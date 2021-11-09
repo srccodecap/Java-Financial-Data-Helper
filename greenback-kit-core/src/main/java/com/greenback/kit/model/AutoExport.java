b4fd2668c5540cd628b8ceda0857874aa179bf4c
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-11-09 21:09:17 +0000

    0.0.43

--- greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExport.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExport.java
@@ -7,4 +7,9 @@ public class AutoExport extends GreenbackObject {
     private String userId;
     private String accountingAccountId;
     private AutoExportParameters parameters;
-    private AutoEx
+    private AutoExportFrequency frequency;
+    private AutoExportState state;
+    private List<String> accountIds;
+    private Instant lastRunAt;
+
+    // from expands
