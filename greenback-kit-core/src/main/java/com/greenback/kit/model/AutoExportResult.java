176b70b932e10a3dc3a482a0e56211698906a6eb
srccodecap <140000899+srccodecap@users.noreply.github.com>
2021-12-17 09:02:03 +0000

    Added missing strings

--- greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExportResult.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/AutoExportResult.java
@@ -1,4 +1,9 @@
 package com.greenback.kit.model;
 
 public class AutoExportResult {
-  
+    private Integer total;
+    private Integer created;
+    private Integer updated;
+    private Integer unmapped;
+    private Integer matched;
+    private Inte
