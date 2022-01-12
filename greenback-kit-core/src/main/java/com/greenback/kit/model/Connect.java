cccae93fb75225f1f87f22289e5b0a70312fc177
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-01-12 21:16:15 +0000

    add some basic tests

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Connect.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Connect.java
@@ -2,4 +2,10 @@ package com.greenback.kit.model;
 
 import java.util.Set;
 
-public class
+public class Connect extends GreenbackObject {
+ 
+    private String label;
+    private ConnectType type;
+    private String name;
+    private ConnectState state;
+  
