79aeccee5f85326c90a8e4336830da96c36ad9cd
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-03-19 21:56:27 +0000

    4th commit

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Contact.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Contact.java
@@ -6,4 +6,14 @@ public class Contact {
     private String domain;
     private String name;
     private String email;
-    
+    private String logoUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNa
