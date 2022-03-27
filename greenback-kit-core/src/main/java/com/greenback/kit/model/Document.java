6c22112b7263532fb8c5bcd271ffaed39f86bbf0
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-03-27 11:33:15 +0000

    Sloader update on _data/NuGet.json

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Document.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Document.java
@@ -3,4 +3,9 @@ package com.greenback.kit.model;
 import java.util.Map;
 import java.util.Set;
 
-public class Document extends 
+public class Document extends GreenbackObject {
+ 
+    protected String referenceId;
+    protected DocumentMeta meta;
+    protected Set<DocumentFlag> flags;
+    protected Map<String,S
