55bc97684c1b6124a7c3a5f70e6b9a18dd9df2b5
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-04-27 11:37:29 +0000

    Fix linting issues

--- greenback-kit-core/src/main/java/com/greenback/kit/model/ExchangeRate.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/ExchangeRate.java
@@ -1,1 +1,9 @@
-package com.greenb
+package com.greenback.kit.model;
+
+public class ExchangeRate {
+    
+    private Double from;
+    private Double rate;
+    private String currencyCode;
+    private Double to;
+    
