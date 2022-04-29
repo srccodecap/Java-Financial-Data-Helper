d6b6db026280ba74ea6aadf9e9e828d9d7a9abf7
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-04-29 13:05:35 +0000

    Update ipc_lista2.05.py

--- greenback-kit-core/src/main/java/com/greenback/kit/model/ExchangeRateType.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/ExchangeRateType.java
@@ -2,4 +2,6 @@ package com.greenback.kit.model;
 
 public enum ExchangeRateType {
 
-    SETTLEMENT,               // if the exchange rate is what was settled t
+    SETTLEMENT,               // if the exchange rate is what was settled to (e.g. paid out in)
+    PRESENTMENT,             // if the exchanger rate is really what was presented in (e.g. marketed as)
+    ESTIMATED;            
