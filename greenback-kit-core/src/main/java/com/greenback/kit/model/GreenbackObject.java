c8477b3c061f0939d4275d64696165be39b15195
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-05-13 08:28:15 +0000

    bump ident

--- greenback-kit-core/src/main/java/com/greenback/kit/model/GreenbackObject.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/GreenbackObject.java
@@ -4,4 +4,12 @@ import java.time.Instant;
 
 public class GreenbackObject {
  
-    protected String 
+    protected String id;
+    protected Instant createdAt;
+    protected Instant updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(Strin
