56f1ca031cc5034a9104479ab8fc04e0648fb798
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-05-25 13:12:48 +0000

    CMakeLists.txt

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Item.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Item.java
@@ -8,4 +8,11 @@ public class Item {
     private String grn;
     private List<String> altGrns;
     private String name;
-    private String descripti
+    private String description;
+    private Double quantity;
+    private Double unitPrice;
+    private Double amount;
+    private String taxGrn;
+    protected Map<String,String> attributes;
+
+   
