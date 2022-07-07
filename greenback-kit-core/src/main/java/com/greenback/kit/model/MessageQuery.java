108d310dec09ce49540da4b4926da5dc01f7e7ef
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-07-07 13:13:11 +0000

    \tmodified:   CO2_analysis/markov_extended.jl

--- greenback-kit-core/src/main/java/com/greenback/kit/model/MessageQuery.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/MessageQuery.java
@@ -4,4 +4,8 @@ import java.time.Instant;
 
 public class MessageQuery extends DocumentQuery<MessageQuery> {
     
-    
+    protected String query;
+    protected Instant minPostedAt;          // start
+    protected Instant maxPostedAt;          // end
+
+    p
