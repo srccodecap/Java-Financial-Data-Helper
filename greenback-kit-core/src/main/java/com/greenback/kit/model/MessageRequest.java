bd311ac6ed17b1b0c37003cb52b735aacd86c44d
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-07-22 12:01:45 +0000

    Add badges to README

--- greenback-kit-core/src/main/java/com/greenback/kit/model/MessageRequest.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/MessageRequest.java
@@ -1,1 +1,10 @@
 package com.greenback.kit.model;
+
+import com.greenback.kit.util.Bytes;
+import java.io.File;
+import java.util.Objects;
+
+public class MessageRequest {
+ 
+    private Boolean async;
+    pr
