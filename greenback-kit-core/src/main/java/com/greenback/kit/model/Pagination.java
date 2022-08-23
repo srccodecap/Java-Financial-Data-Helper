e822ee2e2935a36081d4b0ad478a2fef999270ed
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-08-23 22:02:45 +0000

    Translated using Weblate (Lithuanian)

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Pagination.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Pagination.java
@@ -2,3 +2,10 @@ package com.greenback.kit.model;
 
 public class Pagination {
  
+    private Long limit;
+    private Long count;
+    private Long totalCount;
+    private String previous;
+    private String next;
+
+    public Long getLimit() {
