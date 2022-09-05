1c4df1bfd0ccf16bf35e60db6ef9fbbe52374a9f
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-09-05 11:15:45 +0000

    foot

--- greenback-kit-core/src/main/java/com/greenback/kit/model/PaymentMethod.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/PaymentMethod.java
@@ -1,3 +1,13 @@
 package com.greenback.kit.model;
 
-public enum 
+public enum PaymentMethod {
+    
+    CREDIT_CARD,
+    DEBIT_CARD,
+    REWARD,
+    PAYPAL,
+    GOOGLE_WALLET,
+    GIFT_CARD,
+    MONEY_TRANSFER,
+    CASH,
+    STORE_CREDI
