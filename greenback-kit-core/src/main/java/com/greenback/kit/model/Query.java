403677573645b94a589f23114394e08670a9b9a3
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-10-02 08:27:48 +0000

    Catalan update

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Query.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Query.java
@@ -6,4 +6,10 @@ import static com.greenback.kit.util.Utils.toIterable;
 
 public class Query<T> implements Serializable {
  
-  
+    protected Integer limit;
+    protected Iterable<String> expands;
+    protected String cursor;
+
+    public Integer getLimit() {
+        return limit;
+    
