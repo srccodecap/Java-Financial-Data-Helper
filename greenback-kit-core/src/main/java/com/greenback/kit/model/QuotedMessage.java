7ae1f6afcee5957ae677482a063cee742f649381
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-10-07 22:06:55 +0000

    Fixed some documentation

--- greenback-kit-core/src/main/java/com/greenback/kit/model/QuotedMessage.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/QuotedMessage.java
@@ -3,4 +3,9 @@ package com.greenback.kit.model;
 import java.time.Instant;
 import java.util.List;
 
-public class Q
+public class QuotedMessage {
+    
+    private String subject;
+    private EmailAddress from;
+    private List<EmailAddress> replyTo;
+    private List<EmailA
