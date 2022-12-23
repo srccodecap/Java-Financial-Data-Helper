cb18a3f11db4646fc1546de3e87e5c99962782ec
srccodecap <140000899+srccodecap@users.noreply.github.com>
2022-12-23 13:49:53 +0000

    commit.

--- greenback-kit-core/src/main/java/com/greenback/kit/model/SyncRequest.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/SyncRequest.java
@@ -1,4 +1,12 @@
 package com.greenback.kit.model;
 
 import java.time.Instant;
-im
+import java.util.List;
+import java.util.Map;
+
+public class SyncRequest {
+
+    private Instant from;
+    private Instant to;
+    private Integer limit;
+    private Boolean reu
