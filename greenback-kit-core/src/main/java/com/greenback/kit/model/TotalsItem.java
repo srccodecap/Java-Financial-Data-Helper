6785814fdb9574a6d6399d2b058c1f09cabfb5b3
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-03-04 06:06:26 +0000

    Added notes.

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TotalsItem.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TotalsItem.java
@@ -1,1 +1,8 @@
-package com.green
+package com.greenback.kit.model;
+
+public class TotalsItem {
+    
+    private TotalsItemType type;
+    private String name;
+    private Double amount;
+    private
