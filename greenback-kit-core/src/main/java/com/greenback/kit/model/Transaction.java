8abe99ebecd0df1eb9cded4402a540f9e994b829
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-03-17 09:07:44 +0000

    Delete unneeded file

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Transaction.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Transaction.java
@@ -73,3 +73,138 @@ public class Transaction extends Document {
 
     public String getAccountId() {
         return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getContactId() {
+        return contactId;
+    }
+
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
+
+    public List<Attachment> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<Attachment> attachments) {
+        this.attachments = attachments;
+    }
+
+    public PostalAddress getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(PostalAddress billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public PostalAddress getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(PostalAddress shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public PostalAddress getStoreAddress() {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(PostalAddress storeAddress) {
+        this.storeAddress = storeAddress;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+
+    public List<ExchangeRate> getExchangeRates() {
+        return exchangeRates;
+    }
+
+    public void setExchangeRates(List<ExchangeRate> exchangeRates) {
+        this.exchangeRates = exchangeRates;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
+    }
+
+    public List<Payment> getDeposits() {
+        return deposits;
+    }
+
+    public void setDeposits(List<Payment> deposits) {
+        this.deposits = deposits;
+    }
+
+    public TransactionTotals getTotals() {
+        return totals;
+    }
+
+    public void setTotals(TransactionTotals totals) {
+        this.totals = totals;
+    }
+
+    public Instant getTransactedAt() {
+        return transactedAt;
+    }
+
+    public void setTransactedAt(Instant transactedAt) {
+        this.transactedAt = transactedAt;
+    }
+
+    public Instant getDueAt() {
+        return dueAt;
+    }
+
+    public void setDueAt(Instant dueAt) {
+        this.dueAt = dueAt;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public TransactionExport getExport() {
+        return export;
+    }
+
+    public void setExport(TransactionExport export) {
+        this.export = export;
+    }
+
+}
