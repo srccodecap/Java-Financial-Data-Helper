295c7fdd26f22ba906d2dc310bd49a107fe32c2b
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-04-16 08:19:17 +0000

    usr

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportDeleteMode.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportDeleteMode.java
@@ -1,1 +1,6 @@
-package com.greenback.kit.mo
+package com.greenback.kit.model;
+
+public enum TransactionExportDeleteMode {
+    
+    DEFAULT,                        // must be deleted in accounting before cleared in gb
+    IGNORE_ACCOUNTI
