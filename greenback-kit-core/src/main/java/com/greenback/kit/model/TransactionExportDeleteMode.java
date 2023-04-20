2880d04ec0d48e0c6c713a80bdb72e974c0c1124
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-04-20 22:03:03 +0000

    autofs - remove autofs dentry mount check

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportDeleteMode.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportDeleteMode.java
@@ -4,4 +4,6 @@ public enum TransactionExportDeleteMode {
     
     DEFAULT,                        // must be deleted in accounting before cleared in gb
     IGNORE_ACCOUNTING_FAILURE,      // ignore any failure in accounting delete, then clear in gb
-    SKIP_ACCOUNTING                 // skip deleting in accoun
+    SKIP_ACCOUNTING                 // skip deleting in accounting, just clear in gb
+    
+}
---
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportIntent.java
@@ -0,0 +1,7 @@
+package com.greenback.kit.model;
+
+import java.util.List;
+
+public class TransactionExportIntent {
+ 
+    private String ta
