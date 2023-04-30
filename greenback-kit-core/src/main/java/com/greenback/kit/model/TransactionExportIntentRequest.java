3d5c55846dec6e3a2eb6b3ca1f59cf9e474affe7
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-04-30 08:22:54 +0000

    Updating dotfiles...

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportIntentRequest.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionExportIntentRequest.java
@@ -1,1 +1,7 @@
-package com.greenback.kit
+package com.greenback.kit.model;
+
+import static com.greenback.kit.util.Utils.toIterable;
+import java.time.Instant;
+import java.util.Map;
+
+public class TransactionE
