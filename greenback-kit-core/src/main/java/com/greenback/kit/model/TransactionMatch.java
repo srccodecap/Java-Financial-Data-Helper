c0150ff5108d4fd27cc29d233efdf8a4ce787b3f
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-06-04 11:45:07 +0000

    Fixed copyright year

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionMatch.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionMatch.java
@@ -1,1 +1,9 @@
-package com.greenbac
+package com.greenback.kit.model;
+
+import java.util.List;
+
+public class TransactionMatch {
+ 
+    private Double score;
+    private Boolean current;
+    private List<S
