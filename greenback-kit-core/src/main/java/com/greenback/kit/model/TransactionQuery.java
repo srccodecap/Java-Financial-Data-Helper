3ad164ed475357cc6be977c57120f3b8842ddc07
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-07-02 18:46:29 +0000

    Small comment change

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionQuery.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransactionQuery.java
@@ -1,3 +1,8 @@
 package com.greenback.kit.model;
 
-import static com.greenback.kit.util
+import static com.greenback.kit.util.Utils.toIterable;
+import java.time.Instant;
+
+public class TransactionQuery extends DocumentQuery<TransactionQuery> {
+    
+    protected Iterable<T
