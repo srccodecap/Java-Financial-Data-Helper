23853293bb98c43a74283342e919d7f1a4b984af
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-08-06 08:40:06 +0000

    Bump version: 0.7.2 → 0.7.3

--- greenback-kit-core/src/main/java/com/greenback/kit/model/Transform.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/Transform.java
@@ -3,3 +3,8 @@ package com.greenback.kit.model;
 public class Transform extends GreenbackObject {
  
     private String accountId;
+    private Long sortOrder;
+    private TransformType type;
+    private TransformState state;
+    private String name;
+    private Object
