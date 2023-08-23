7dfc20395cf2c554219f8f37383cd6a75dd849ad
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-08-23 18:45:24 +0000

    "Initial Commit"

--- greenback-kit-core/src/main/java/com/greenback/kit/model/TransformQuery.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/TransformQuery.java
@@ -2,4 +2,8 @@ package com.greenback.kit.model;
 
 import java.time.Instant;
 
-public class TransformQuery ex
+public class TransformQuery extends Query<TransformQuery> {
+
+    protected Iterable<String> accountIds;
+    protected Iterable<TransformState> states;
+    protected Instant min
