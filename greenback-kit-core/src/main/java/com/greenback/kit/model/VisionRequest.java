0fcc8b9da284a670e5b4f2b0f1c722fd683f4323
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-10-11 05:55:13 +0000

    Update extensions.

--- greenback-kit-core/src/main/java/com/greenback/kit/model/VisionRequest.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/model/VisionRequest.java
@@ -1,3 +1,10 @@
 package com.greenback.kit.model;
 
-import com.greenba
+import com.greenback.kit.util.Bytes;
+import java.io.File;
+import java.util.Objects;
+
+public class VisionRequest {
+ 
+    private Boolean async;
+    
