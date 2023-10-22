523bdb40f65f7f781b2c01a4b32319dcc1c107a6
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-10-22 07:33:29 +0000

    Updated android-development-notes.md

--- greenback-kit-core/src/main/java/com/greenback/kit/util/Bytes.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/util/Bytes.java
@@ -1,1 +1,11 @@
 package com.greenback.kit.util;
+
+import java.io.File;
+
+public class Bytes {
+ 
+    private File file;
+    private byte[] bytes;
+    private String name;
+
+    public File getF
