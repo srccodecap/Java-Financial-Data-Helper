b2de961e0c0e2b8c8d743052899a0e1e2575029e
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-11-18 11:24:52 +0000

    added version info

--- greenback-kit-core/src/main/java/com/greenback/kit/util/IoFunction.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/util/IoFunction.java
@@ -4,4 +4,6 @@ import java.io.IOException;
 
 public interface IoFunction<P,R> {
     
-    R apply(P p) th
+    R apply(P p) throws IOException;
+    
+}
---
+++ greenback-kit-core/src/main/java/com/greenback/kit/util/StreamingPaginated.java
@@ -0,0 +1,5 @@
+package com.greenback.kit.util;
+
+import com.greenback.kit.model.Paginated;
+import com.greenback.kit.util.IoFunction;
+import java.io
