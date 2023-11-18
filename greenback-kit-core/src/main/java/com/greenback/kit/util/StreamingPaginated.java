8d87fb43b8bc4df50e4419cff8badcd468f39cc3
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-11-18 12:34:57 +0000

    initial development

--- greenback-kit-core/src/main/java/com/greenback/kit/util/StreamingPaginated.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/util/StreamingPaginated.java
@@ -2,4 +2,8 @@ package com.greenback.kit.util;
 
 import com.greenback.kit.model.Paginated;
 import com.greenback.kit.util.IoFunction;
-import java.io
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Iterator;
+import static java.util.Optional.ofNullable;
+import java.util
