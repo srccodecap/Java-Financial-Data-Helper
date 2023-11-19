0b19cd616d5d6974bdb982714f594a429c4a780d
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-11-19 05:40:19 +0000

    new unit test

--- greenback-kit-core/src/main/java/com/greenback/kit/util/StreamingPaginated.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/util/StreamingPaginated.java
@@ -6,4 +6,8 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Iterator;
 import static java.util.Optional.ofNullable;
-import java.util
+import java.util.concurrent.atomic.AtomicReference;
+
+public class StreamingPaginated<T> extends Paginated<T> {
+
+    private IoFunction<Paginated<T>,Paginated
