691f1d40292c854f430fbc59d2ba23c9fff98e08
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-12-17 18:05:56 +0000

    less

--- greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionAnnotation.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionAnnotation.java
@@ -4,3 +4,10 @@ import java.util.List;
 
 public class VisionAnnotation {
  
+    private String id;
+    private String text;
+    private List<VisionRectangle> bounds;
+    private List<VisionTextSpan> spans;
+    
+    public String getType() {
+    
