9c17569b7998efa5e58ec531d044f9566277568b
srccodecap <140000899+srccodecap@users.noreply.github.com>
2023-12-26 11:18:17 +0000

    Update symbols

--- greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionAnnotations.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionAnnotations.java
@@ -1,3 +1,9 @@
 package com.greenback.kit.vision;
 
 import java.util.List;
+
+public class VisionAnnotations {
+
+    private VisionAnnotation fullText;
+    private List<VisionAnnotation> texts;
+    private List<VisionAnnotati
