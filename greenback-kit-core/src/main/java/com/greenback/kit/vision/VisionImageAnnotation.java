1958c2c2c071e52259c7a98c89c1938d734a418d
srccodecap <140000899+srccodecap@users.noreply.github.com>
2024-01-15 13:29:06 +0000

    release v1.0.1

--- greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionImageAnnotation.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionImageAnnotation.java
@@ -1,3 +1,10 @@
 package com.greenback.kit.vision;
 
-public class VisionImageAnnotation extends VisionAnnotati
+public class VisionImageAnnotation extends VisionAnnotation {
+    
+    private String md5;
+    private Integer naturalWidth;
+    private Integer naturalHeight;
+
+    @Override
+    public String get
