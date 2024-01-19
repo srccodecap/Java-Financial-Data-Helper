fa43707be102245ec3b9d5f6018956f4be5a6891
srccodecap <140000899+srccodecap@users.noreply.github.com>
2024-01-19 10:10:40 +0000

    type fixed

--- greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionRectangle.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionRectangle.java
@@ -54,3 +54,56 @@ public class VisionRectangle {
     @Override
     public String toString() {
         if (this.x == null && this.y == null && this.w == null && this.h == null) {
+            return null;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.x != null ? this.x : "");
+        sb.append(",");
+        sb.append(this.y != null ? this.y : "");
+        sb.append(" ");
+        sb.append(this.w != null ? this.w : "");
+        sb.append("x");
+        sb.append(this.h != null ? this.h : "");
+        
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.x);
+        hash = 67 * hash + Objects.hashCode(this.y);
+        hash = 67 * hash + Objects.hashCode(this.w);
+        hash = 67 * hash + Objects.hashCode(this.h);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final VisionRectangle other = (VisionRectangle) obj;
+        if (!Objects.equals(this.x, other.x)) {
+            return false;
+        }
+        if (!Objects.equals(this.y, other.y)) {
+            return false;
+        }
+        if (!Objects.equals(this.w, other.w)) {
+            return false;
+        }
+        if (!Objects.equals(this.h, other.h)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionTemporalAnnotation.java
@@ -0,0 +1,6 @@
+package com.greenback.kit.vision;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Vis
