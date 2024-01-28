bf56600583a5a3b0766f2ccac0cbedddf56351a8
srccodecap <140000899+srccodecap@users.noreply.github.com>
2024-01-28 08:35:26 +0000

    Updated Image

--- greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionTextSpan.java
+++ greenback-kit-core/src/main/java/com/greenback/kit/vision/VisionTextSpan.java
@@ -1,1 +1,12 @@
 package com.greenback.kit.vision;
+
+public class VisionTextSpan {
+ 
+    private Integer s;
+    private Integer e;
+
+    public Integer getS() {
+        return s;
+    }
+
+    publ
