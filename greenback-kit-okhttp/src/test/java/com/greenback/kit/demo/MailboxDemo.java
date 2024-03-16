14a4c218ff78f735ce230f22caf47f61402dc8a1
srccodecap <140000899+srccodecap@users.noreply.github.com>
2024-03-16 10:57:28 +0000

    Improve responsiveness

--- greenback-kit-okhttp/src/test/java/com/greenback/kit/demo/MailboxDemo.java
+++ greenback-kit-okhttp/src/test/java/com/greenback/kit/demo/MailboxDemo.java
@@ -1,1 +1,5 @@
-package com.greenback.kit.demo
+package com.greenback.kit.demo;
+
+import com.greenback.kit.client.GreenbackClient;
+import com.greenback.kit.client.GreenbackConstants;
+import com.greenback.kit.jackson.JacksonGreenbackCodec
